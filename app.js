7ba62fc4afcf165869ad30c6b002937e18de226b
houssein88 <fadhirikah92@gmail.com>
2021-07-01 17:26:20 +0000

    update1

---
+++ app.js
@@ -0,0 +1,3 @@
+const { diff } = require(".")
+const date = new Date();
+console.log(diff(date, '1994-10-12'));
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 exports.diff = (date1, date2) => {
-    if (typeof date1 != String || typeof date2 != String) {
-        return "Les dates doivent être en string !"
-    }
-
     var t1 = new Date(date1);
     var t2 = new Date(date2);
-    var diffSeconde = Math.abs(t1.getTime() / 1000 - t2.getTime() / 1000); // Difference en Seconde
+    var diffMilliSeconde = Math.abs(t1.getTime() - t2.getTime()); // Difference en milliSeconde
+    var diffSeconde = diffMilliSeconde / 1000; // Difference en Seconde
     var diffMinute = diffSeconde / 60; // Difference en Minute
     var diffHeure = diffMinute / 60; // Difference en Heure
     var diffJour = diffHeure / 24;
+    var diffYears = diffJour / 365;
     difference = {
-        seconds: diffSeconde,
-        minutes: diffMinute,
-        hours: diffHeure,
-        days: diffJour
-    }
-    console.log(difference);
+            millisecondes: diffMilliSeconde,
+            seconds: diffSeconde,
+            minutes: diffMinute,
+            hours: diffHeure,
+            days: diffJour,
+            years: diffYears
+        }
+        // console.log(difference);
     return difference;
 }
